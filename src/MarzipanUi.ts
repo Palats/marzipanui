dc89f18f145c4729929264557b28ce65a4b99312
Pierre Palatin <pierre@palatin.fr>
2020-07-25 18:51:55 +0200

    Allow modification of a parameter

--- src/MarzipanUi.ts
+++ src/MarzipanUi.ts
@@ -1,24 +1,52 @@
 import { LitElement, html, css, property, customElement } from 'lit-element';
 import '@material/mwc-top-app-bar';
 import '@material/mwc-drawer';
-import { Drawer } from '@material/mwc-drawer';
 import '@material/mwc-icon-button';
+import '@material/mwc-textfield';
+import { Drawer } from '@material/mwc-drawer';
+
+class Parameters {
+  public address = 'http://localhost:8080'
+  public left = -2.0;
+  public right = 1.0;
+
+  url() {
+    const q = new URLSearchParams();
+    q.set('left', this.left.toString());
+    return this.address + '?' + q.toString();
+  }
+}
 
 @customElement('marzipan-ui')
 export class MarzipanUi extends LitElement {
 
-  @property({ type: String }) page = 'main';
+  @property({ type: String })
+  page = 'main';
 
-  @property({ type: String }) title = 'plop2';
+  @property({ type: String })
+  title = 'plop2';
+
+  @property({ type: String })
+  private targetURL = '';
+
+  private params = new Parameters();
 
   static styles = css``
+
+  constructor() {
+    super();
+    this.updateURL();
+  }
 
   render() {
     return html`
     <main>
       <mwc-drawer type="dismissible" id="drawer">
         <div>
-          <p>Drawer Content!</p>
+          <h4>Position</h4>
+          <div>
+            <mwc-textfield label="Left" value="-2.0" endaligned @change="${this.handleLeft}"></mwc-textfield>
+          </div>
         </div>
         <div slot="appContent">
           <mwc-top-app-bar id="appbar">
@@ -26,7 +54,7 @@ export class MarzipanUi extends LitElement {
             <div slot="title">Marzipan</div>
           </mwc-top-app-bar>
           <div>
-            <p>Main Content!</p>
+            <img src="${this.targetURL}" alt="generated fractal" id="render"/>
           </div>
         </div>
       </mwc-drawer>
@@ -35,11 +63,22 @@ export class MarzipanUi extends LitElement {
   }
 
   firstUpdated(changedProperties: any) {
-    const drawer = <Drawer>this.shadowRoot?.getElementById("drawer");
-    if (drawer) {
-      this.shadowRoot?.addEventListener('MDCTopAppBar:nav', () => {
-        drawer.open = !drawer.open;
-      });
-    }
+    const drawer = this.shadowRoot?.getElementById('drawer') as Drawer;
+    drawer.open = true;
+    this.shadowRoot?.addEventListener('MDCTopAppBar:nav', () => {
+      drawer.open = !drawer.open;
+    });
+  }
+
+  handleLeft(event: Event) {
+    if (!event.target) { return }
+    const value = (event.target as HTMLInputElement).value
+    this.params.left = parseFloat(value);
+    this.updateURL();
+  }
+
+  updateURL() {
+    this.targetURL = this.params.url();
   }
 }
+
